979f4f2e509677f9bc1426fc30bce91156e1c913
Yigal Duppen <yigal@noumenadigital.com>
2024-06-11 13:42:38 +0200

    Have keycloak provisioning work & tie in to engine

--- keycloak-provisioning/local.sh
+++ keycloak-provisioning/local.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 
 set -o errexit
 set -o nounset
@@ -8,7 +8,5 @@ export KEYCLOAK_USER=${KEYCLOAK_USER?: "KEYCLOAK_USER has not been set" }
 export KEYCLOAK_PASSWORD=${KEYCLOAK_PASSWORD?: "KEYCLOAK_PASSWORD has not been set" }
 export KEYCLOAK_URL=${KEYCLOAK_URL?: "KEYCLOAK_URL has not been set" }
 export TF_VAR_default_password=${TF_VAR_default_password?: "TF_VAR_default_password has not been set"}
-export TF_VAR_app_name=${TF_VAR_app_name?: "TF_VAR_app_name has not been set"}
 
-terraform init
 terraform apply -auto-approve -state=state.tfstate
