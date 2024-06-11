c5d29e5ab9d0a8f104576e9a74dce8e5ada6e838
Yigal Duppen <yigal@noumenadigital.com>
2024-06-11 14:53:09 +0200

    Include Python

--- python/src/config.py
+++ python/src/config.py
@@ -1,6 +1,8 @@
 
-TOKEN_URL = "https://keycloak-pwctraining-nplintegrations.noumena.cloud/realms/nplintegrations/protocol/openid-connect/token"
-ROOT_URL = "https://engine-pwctraining-nplintegrations.noumena.cloud"
+# TOKEN_URL = "https://keycloak-pwctraining-nplintegrations.noumena.cloud/realms/nplintegrations/protocol/openid-connect/token"
+# ROOT_URL = "https://engine-pwctraining-nplintegrations.noumena.cloud"
+TOKEN_URL = "http://keycloak:11000/realms/nplintegrations/protocol/openid-connect/token"
+ROOT_URL = "https://engine:12000"
 REALM = "nplintegrations"
 USERNAME = "bob"
 PASSWORD = "bob"
