49f2079d0bf4f2b75beb4932ffac35674adbf5c5
Jean <jean@noumenadigital.com>
2024-07-29 08:02:32 +0200

    refined nginx conf with cors file

--- python/src/stream.py
+++ python/src/stream.py
@@ -83,6 +83,7 @@ class StreamReader:
                 exit()
 
     def manage_notification(self, event: dict):
+        print(event)
         notification = Notification(**event)  # type: ignore
         if notification.name == REPAYMENT_OCCURRENCE_NAME:
             self.manage_repayment_occurrence(notification)
