065991b1b9929ef3240f8d39de250e9f80215efc
Jean <jean@noumenadigital.com>
2024-09-20 17:12:53 +0200

    some progress

--- local.mk
+++ local.mk
@@ -6,3 +6,15 @@ KEYCLOAK_HEALTH_URL?=http://localhost:9000
 health-check:
 	curl -v $(KEYCLOAK_HEALTH_URL)/health
 	curl -v $(ENGINE_URL)/actuator/health
+
+.PHONY: install
+install:
+	mvn $(MAVEN_CLI_OPTS) install
+	docker compose build
+
+.PHONY:	run-only
+run-only:
+	docker compose up
+
+.PHONY:	run
+run: install run-only
