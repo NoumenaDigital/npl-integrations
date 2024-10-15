16d5ddd05d3a2d30b1c6c8233a882fc0059fefef
Jean <jean@noumenadigital.com>
2024-10-15 10:26:20 +0200

    frame of stream mocking working

--- python-listener/test/unit_test.py
+++ python-listener/test/unit_test.py
@@ -1,7 +1,11 @@
 from nose.tools import assert_is_not_none, assert_equal
-from unittest.mock import patch
+from unittest.mock import patch, MagicMock
+from unittest import mock
+
+from requests_sse import MessageEvent
 
 from src.auth import AuthService
+from src import stream
 
 
 def raise_for_status():
@@ -31,3 +35,25 @@ def test_auth(mock_post):
 
     assert_is_not_none(access_token)
     assert_equal(access_token, my_access_token)
+
+# @patch('src.stream.StreamReader.get_stream')
+def test_stream(): # mock_get
+
+    stream_reader = stream.StreamReader(None)
+
+    with mock.patch('src.stream.StreamReader.get_stream') as mock_f:
+        mock_f.return_value.__enter__.return_value = iter([
+            MessageEvent(
+                type="notify",
+                data='{"x": 1}',
+                origin="my origin",
+                last_event_id="my last_event_id"
+            ),
+            2,
+            3
+        ])
+
+        for event in stream_reader.read_stream(""):
+            print(event)
+            assert_equal(event, "not likely to be this")
+        assert_equal(1, 1)
