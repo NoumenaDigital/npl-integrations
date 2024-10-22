3494a81367094bf37bd8ca91990dc70d35076452
Jean <jean@noumenadigital.com>
2024-10-22 15:37:42 +0200

    added bash client config to install step

--- local.mk
+++ local.mk
@@ -4,6 +4,7 @@ MAVEN_CLI_OPTS?=-s .m2/settings.xml --no-transfer-progress
 .PHONY: install
 install:
 	mvn $(MAVEN_CLI_OPTS) install
+	chmod +x bash/client.sh
 	make -f local.mk build-images
 
 .PHONY: build-images
