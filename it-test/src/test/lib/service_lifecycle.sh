e554380ccf4bef4c7f6ca16849ba167598b5c336
Jean <jean@noumenadigital.com>
2024-10-29 14:57:43 +0100

    Added information print

--- it-test/src/test/lib/service_lifecycle.sh
+++ it-test/src/test/lib/service_lifecycle.sh
@@ -1,8 +1,9 @@
 run_services() {
 	local app_name_clean=$1
 
+	echo "Starting listener service for $app_name_clean"
 	. ./venv/bin/activate
-	REALM="$app_name_clean" python python-listener/app.py &
+	REALM="$app_name_clean" python3 python-listener/app.py &
     echo $!
 }
 
