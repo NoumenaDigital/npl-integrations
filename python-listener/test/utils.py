84375c047bb9cbd08600291572579317519dcafa
Jean <jean@noumenadigital.com>
2024-12-04 13:23:20 +0100

    python-listener removed unused imports

--- python-listener/test/utils.py
+++ python-listener/test/utils.py
@@ -1,15 +1,3 @@
-import json
-import time
-
-from nose.tools import assert_is_not_none, assert_equal, assert_true
-from unittest.mock import patch
-from requests_sse import MessageEvent
-
-import app
-from src.auth import AuthService
-from src.stream import Notification, REPAYMENT_OCCURRENCE_NAME, StreamReader
-
-
 def raise_for_status():
     pass
 
