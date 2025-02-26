97148bf718714cb9c96a2c640d217a914dc8f4d6
jean-noumena <97169085+jean-noumena@users.noreply.github.com>
2025-02-26 14:19:31 +0100

    DVX-193 fix docker setup (#55)

--- streamlit-ui/src/config.py
+++ streamlit-ui/src/config.py
@@ -6,8 +6,15 @@ load_dotenv()
 
 ORG = os.getenv("VITE_NC_ORG_NAME") if os.getenv("VITE_NC_ORG_NAME") is not None else "training"
 REALM = os.getenv("VITE_NC_APP_NAME") if os.getenv("VITE_NC_APP_NAME") is not None else "nplintegrations"
-NOUMENA_DOMAIN = f"{ORG}-{REALM}.noumena.cloud"
-KEYCLOAK_DOMAIN = f"keycloak-{NOUMENA_DOMAIN}"
-TOKEN_URL = f"https://{KEYCLOAK_DOMAIN}/realms/{REALM}/protocol/openid-connect/token"
-USER_INFO_URL = f"https://{KEYCLOAK_DOMAIN}/realms/{REALM}/protocol/openid-connect/userinfo"
-ROOT_URL = f"https://engine-{NOUMENA_DOMAIN}"
+DOMAIN = os.getenv("DOMAIN") if os.getenv("DOMAIN") is not None else "noumena.cloud"
+LOCAL_TOKEN_URL = f"""http://keycloak:11000/realms/{REALM}/protocol/openid-connect/token"""
+LOCAL_ROOT_URL = "http://engine:12000"
+LOCAL_USER_INFO_URL = f"""http://keycloak:11000/realms/{REALM}/protocol/openid-connect/userinfo"""
+CLOUD_TOKEN_URL = f"""https://keycloak-{ORG}-{REALM}.{DOMAIN}/realms/{REALM}/protocol/openid-connect/token"""
+CLOUD_ROOT_URL = f"""https://engine-{ORG}-{REALM}.{DOMAIN}"""
+CLOUD_USER_INFO_URL = f"""https://keycloak-{ORG}-{REALM}.{DOMAIN}/realms/{REALM}/protocol/openid-connect/userinfo"""
+
+local = os.getenv("ENV") == "LOCAL"
+TOKEN_URL = LOCAL_TOKEN_URL if local else CLOUD_TOKEN_URL
+USER_INFO_URL = LOCAL_USER_INFO_URL if local else CLOUD_USER_INFO_URL
+ROOT_URL = LOCAL_ROOT_URL if local else CLOUD_ROOT_URL
