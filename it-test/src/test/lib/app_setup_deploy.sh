7e8abd4b6fb6cf8138ea60bb0daeb1addb9e3f7c
Daniela <daniela.gianora@noumenadigital.com>
2025-12-05 07:36:22 +0100

    enabled cloud clear

--- it-test/src/test/lib/app_setup_deploy.sh
+++ it-test/src/test/lib/app_setup_deploy.sh
@@ -47,7 +47,7 @@ setup_deploy() {
 	local app_slug=$1
 	local realm_url=$2
 
-	# npl cloud clear --tenant "$tenant_slug" --app "$app_slug" TODO - re-enable when we can clear apps with service accounts
+	 npl cloud clear --tenant "$tenant_slug" --app "$app_slug"
 	npl cloud deploy npl --tenant "$tenant_slug" --app "$app_slug" --migration ./npl/src/main/migration.yml
 
     # populate_iam "$app_slug" "$realm_url" # TODO - re-enable when we can create apps & users
